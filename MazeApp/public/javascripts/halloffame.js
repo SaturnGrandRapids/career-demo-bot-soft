f6cc7f977280061b9f4292db719efeda7f5e5b35
Nathan Metelak <nmetelak@saturnsys.com>
2015-02-09 16:09:10 -0600

    Refactoring hall of fame to use require.js

--- MazeApp/public/javascripts/halloffame.js
+++ MazeApp/public/javascripts/halloffame.js
@@ -1,29 +1,47 @@
-var scoreboardData = [];
-var socket = io();
 
-socket.on('game over',function(msg){
-    var isUpdate = false;
-    $(scoreboardData).each(function(){
-        if(this.username === msg.username){
-            this.points = msg.points;
-            this.moves = msg.moves;
-            this.mazeHtml = msg.mazeHtml;
-            this.playerName = msg.playerName
-            isUpdate = true;
+require.config({
+    // The shim config allows us to configure dependencies for
+    // scripts that do not call define() to register a module
+    shim: {
+        'socketio': {
+            exports: 'io'
         }
-    });
-    if(!isUpdate){
-        scoreboardData.push(msg);
+    },
+    paths: {
+        jquery: 'lib/jquery-1.8.2',
+        socketio: '../socket.io/socket.io'
     }
-    rebuildHallOfFame();
 });
-//TODO: Sort these results and present them in a nicer layout
-function rebuildHallOfFame(){
-    $('.halloffame').empty();
-    $('.halloffame').append('<h1>The Amazing Maze Challenge Hall of Fame</h1>');
- //   $('.halloffame').append('<div class="halltable">');
-    $(scoreboardData).each(function(){
-        $('.halloffame').append('<tr><td text-align:left>' + this.playerName + '</td><td align="right">' + this.points + '</td><td align="right">' + this.moves + '</td><td>       ' + this.username     +'</td></tr>');
+
+require(['jquery','socketio'], function($, io) {
+
+    var scoreboardData = [];
+    var socket = io();
+
+    socket.on('game over', function (msg) {
+        var isUpdate = false;
+        $(scoreboardData).each(function () {
+            if (this.username === msg.username) {
+                this.points = msg.points;
+                this.moves = msg.moves;
+                this.mazeHtml = msg.mazeHtml;
+                this.playerName = msg.playerName
+                isUpdate = true;
+            }
+        });
+        if (!isUpdate) {
+            scoreboardData.push(msg);
+        }
+        rebuildHallOfFame();
     });
- //   $('.halloffame').append('</div>');
-}
+//TODO: Sort these results and present them in a nicer layout
+    function rebuildHallOfFame() {
+        $('.halloffame').empty();
+        $('.halloffame').append('<h1>The Amazing Maze Challenge Hall of Fame</h1>');
+        //   $('.halloffame').append('<div class="halltable">');
+        $(scoreboardData).each(function () {
+            $('.halloffame').append('<tr><td text-align:left>' + this.playerName + '</td><td align="right">' + this.points + '</td><td align="right">' + this.moves + '</td><td>       ' + this.username + '</td></tr>');
+        });
+        //   $('.halloffame').append('</div>');
+    }
+});
